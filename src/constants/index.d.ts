72c7ba3c695cab10d75203200ce00e8f7459db0b
Tunz1608 <tuan.skipli@gmail.com>
2026-01-07 19:59:09 +0700

    Add theme provider and enhance styling with Tailwind CSS integration

---
+++ src/constants/index.d.ts
@@ -0,0 +1,147 @@
+export const PRODUCT = {
+    title: "New Maximum Potency Formula - Lymphatic Drainage Capsules by Sculptique™",
+    description: "",
+    rating: {
+        count: 4.8,
+        type: "Excellent",
+    },
+    sub_details: [
+        {
+            label: "Join over 93 Thousand who say - it WORKS!",
+            icon: ""
+        },
+        {
+            label: "Restores your body’s natural 24-hour lymphatic cycle",
+            icon: ""
+        },
+        {
+            label: "Helps reduce fluid retention and the appearance of puffiness and bloating",
+            icon: ""
+        },
+        {
+            label: "Helps fall asleep faster, stay asleep longer and wake up energized",
+            icon: ""
+        },
+        {
+            label: "Eliminates joint stiffness, pain, morning creakiness and feel more grounded",
+            icon: ""
+        },
+        {
+            label: "Boosts energy, mental clarity and emotional balance",
+            icon: ""
+        }
+    ],
+    images: [
+        "/product-image-1.png",
+        "/product-image-2.png",
+        "/product-image-3.png"
+
+    ],
+    packages: [
+        {
+            label: "1 Bottle",
+            description: "1 bottle total",
+            price: 59.95,
+            savings: 27.99,
+            details: [
+                "Free USA Shipping",
+            ],
+            featured: ""
+        },
+        {
+            label: "Buy 2 get 1 free",
+            description: "3 bottles total",
+            price: 179.85,
+            savings: 115.93,
+            details: [
+                "Free USA Shipping",
+                "Free Anti-Bloating Protocol E-book"
+            ],
+            featured: "New Year Sale"
+        },
+        {
+            label: "Buy 3 Get 2 Free",
+            description: "5 bottles total",
+            price: 299.75,
+            savings: 203.67,
+            details: [
+                "Free USA Shipping",
+                "Free Anti-Bloating Protocol E-book",
+                "$20 Gift Card",
+            ],
+            featured: "",
+        }
+    ]
+}
+
+export const REVIEW = [
+    {
+        name: "Margaret Ellison | FL",
+        rate_count: 5,
+        isVerified: true,
+        title: "Even my husband noticed... the spark came back.",
+        description: "After years of hiding under coverups, picking apart my body, and feeling disconnected, I finally feel at ease again. I feel less bloated, lighter, like my body is working with me not against me. These past months, I’ve been present. Even my marriage feels renewed, not just in how I look but in how I feel. It’s hard to explain, but once you try it you’ll get it. I’d recommend this to my closest friends without question."
+    }
+]
+
+export const FAQ = [
+    {
+        label: "Ingredients proven by science",
+        sub: {
+            title: "Sculptique Ingredients:",
+            items: [
+                {
+                    name: "Echinacea purpurea Extract",
+                    description: "Known for its anti-inflammatory properties, it may support skin health."
+                },
+                {
+                    name: "Dandelion Extract",
+                    description: "Traditionally used as a diuretic, it may help reduce water retention."
+                },
+                {
+                    name: "Burdock Powder",
+                    description: "Contains antioxidants that may promote skin clarity."
+                },
+                {
+                    name: "Cleavers Extract",
+                    description: "Believed to support lymphatic drainage and detoxification."
+                },
+                {
+                    name: "Rutin",
+                    description: "A flavonoid that may strengthen blood vessels and improve circulation."
+                },
+                {
+                    name: "Bromelain Powder",
+                    description: "An enzyme from pineapple that may reduce inflammation and support tissue repair."
+                },
+                {
+                    name: "Lemon Powder",
+                    description: "Rich in vitamin C, it may aid in collagen production and skin rejuvenation."
+                },
+                {
+                    name: "Kelp Extract",
+                    description: "A source of iodine and minerals that may support skin metabolism."
+                }
+            ],
+            footer: "These natural ingredients work together to reduce puffiness, bloating, fluid retention."
+        }
+    },
+    {
+        label: "How does it actually work?",
+        sub: {
+            descriptions: [
+                "Sculptique works by improving blood flow and supporting lymphatic drainage to reduce fluid buildup that causes puffiness, inflammation, and water retention. It also reduces inflammation and boosts collagen production to help skin become firmer and smoother."
+            ]
+        }
+    },
+    {
+        label: "Shipping and returns",
+        sub: {
+            descriptions: [
+                "All of Sculptique orders get FREE shipping straight from our USA warehouse. Orders are usually shipped out within 1-2 working days, and you should receive the order within 3-7 working days for domestic USA orders, and within 10 working days for International orders.",
+                "We also offer a 60-day money back guarantee - if you are unsatisfied with our product, you can take advantage of our guarantee and ship back the product to us to get your return within 60 days of receiving your order."
+            ]
+        }
+    },
+
+]
